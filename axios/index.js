44797cc3f067723a39f02f3c53720e56c9cd92a6
jincong <876023101@qq.com>
2022-11-18 16:10:51 +0800

    初始化获取黄金价格

---
+++ axios/index.js
@@ -0,0 +1,46 @@
+const axios = require("axios");
+let uaTool = require("useragent-tool");
+let randomUserAgent = uaTool.getRandomUserAgent();
+
+const hjUrl = `https://hq.sinajs.cn/etag.php?_=${Date.now()}&list=hf_XAU`;
+
+const getHJ = () => {
+  return new Promise((resolve, reject) => {
+    axios
+      .get(hjUrl, {
+        headers: {
+          "User-Agent": randomUserAgent,
+          "Cache-Control": "no-store",
+          Accept: "*/*",
+          "Accept-Encoding": "gzip, deflate, br",
+          "Accept-Language": "zh-CN,zh;q=0.9,or;q=0.8,ar;q=0.7",
+          Connection: "keep-alive",
+          Host: "hq.sinajs.cn",
+          "If-None-Match": 'W/"ICiAVdjdzrI"',
+          Referer: "https://finance.sina.com.cn/futures/quotes/XAU.shtml",
+          "sec-ch-ua":
+            '"Google Chrome";v="107", "Chromium";v="107", "Not=A?Brand";v="24"',
+          "sec-ch-ua-mobile": "?0",
+          "sec-ch-ua-platform": "macOS",
+          "Sec-Fetch-Dest": "script",
+          "Sec-Fetch-Mode": "no-cors",
+          "Sec-Fetch-Site": "cross-site",
+          "User-Agent":
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36",
+        },
+      })
+      .then((res) => {
+        console.log({ res: res?.data });
+        let price = res.data.split('"')[1].substring(0, 7);
+        resolve(price);
+      })
+      .catch((e) => {
+        console.log({ getHjError: e });
+        reject(e);
+      });
+  });
+};
+
+module.exports = {
+  getHJ,
+};
