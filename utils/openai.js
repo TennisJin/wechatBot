d29e9df58634db1c9d5196363f0fc0f8228af877
jincong <876023101@qq.com>
2022-12-15 22:35:14 +0800

    refactor: 输出key 提交人：jincong

--- utils/openai.js
+++ utils/openai.js
@@ -1,5 +1,5 @@
 const { Configuration, OpenAIApi } = require("openai");
-console.log({ apiKey: process.env });
+console.log({ apiKey: process.env.OPENAI_API_KEY });
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
